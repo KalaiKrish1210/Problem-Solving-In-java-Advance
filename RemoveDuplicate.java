0fa19c9b8f7f8223700dbc3250d6394d9272db13
KALAI SELVAN KRISHNAMOORTHY <76065108+KalaiKrish1210@users.noreply.github.com>
2021-04-22 18:26:13 +0530

    Add files via upload

---
+++ RemoveDuplicate.java
@@ -0,0 +1,69 @@
+package dili;
+
+import java.util.Scanner;
+
+class removedup {
+	// class declaration
+	public void perform(int[] arr) {
+		// Method declaration
+
+		// If you need to find the duplicate in array we have to create the two "for"
+		// loop like same but different index assigning for purpose of comparing two
+		// indexed array
+
+		for (int a = 0; a < arr.length; a++) {
+			// First loop a
+			int count = 0;
+			// initialize the count variable why? beacause, the count will be zero until the
+			// (b- for) "if condition" not satifying. If satisfied the "if" condition the
+			// proceed further operation
+			for (int b = a + 1; b < arr.length; b++) {
+				// Second loop b
+				if (arr[a] == arr[b]) {
+					// Compare the two indexed value in the array
+					count++;
+					// If satisfied the "if" condition the proceed further operation is count
+					// getting increased count greater than 0 and assign the arr[b] = o to avoid the
+					// visiting value again.
+					arr[b] = 0;
+				}
+			}
+			// Comparing if the count is Greater than zero and arr[a] not equal to 0
+			if (count > 0 && arr[a] != 0) {
+				System.out.println("Duplicate value  \t" + arr[a]);
+				// Print the duplicate value
+			}
+			// Comparing if the count is equal to the zero and arr[a] not equal to 0
+			if (count == 0 && arr[a] != 0) {
+				System.out.println("Remaining  Values\t" + arr[a]);
+				// Print the duplicate value
+			}
+		}
+
+	}
+}
+
+public class RemoveDuplicate {
+
+	public static void main(String[] args) {
+		// Main function
+		removedup obj = new removedup();
+		// object creation
+		Scanner input = new Scanner(System.in);
+		// Scanner method syntax
+		System.out.println("enter how many elements do you want in this array:");
+		int n = input.nextInt();
+		// Getting the Array size value from user
+		int[] arr = new int[n];
+		// Array declaration
+		System.out.println("enter all the elements:");
+		for (int i = 0; i < n; i++) {
+			arr[i] = input.nextInt();
+			// Getting the Array value from user
+		}
+		obj.perform(arr);
+		// Method calling
+
+	}
+
+}
